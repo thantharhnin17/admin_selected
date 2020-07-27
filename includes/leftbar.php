6dca331814f6d70574ca89a7db5396ef54cf9be5
thantharhnin17 <thanthark88@gmail.com>
2020-07-26 22:24:15 -0800

    added includes file

---
+++ includes/leftbar.php
@@ -0,0 +1,29 @@
+ <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
+    <div class="main-menu-content">
+      <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+        <li class=" nav-item"><a href="dashboard.php"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span></a>
+        </li>
+
+        
+       
+<li class=" nav-item"><a href="#"><i class="la la-file"></i><span class="menu-title" data-i18n="nav.footers.main">Admission application</span></a>
+          <ul class="menu-content">
+            <li><a class="menu-item" href="pending-application.php" data-i18n="nav.footers.footer_light">Pending</a>
+            </li>
+            <li><a class="menu-item" href="selected-application.php" data-i18n="nav.footers.footer_light">Selected/Verified</a>
+            </li>
+            <li><a class="menu-item" href="rejected-application.php" data-i18n="nav.footers.footer_dark">Rejected</a>
+            </li>
+         
+          </ul>
+        </li>
+     
+<li class=" nav-item"><a href="search-application.php"><i class="la la-search"></i><span class="menu-title" data-i18n="nav.dash.main">Search Application</span></a>
+        </li>
+  
+
+
+   
+      </ul>
+    </div>
+  </div>
